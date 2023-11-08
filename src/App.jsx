0a9c2a0779aac95bd97eff032a27f04686a9d5bc
Hugo Pereira <hugocastroferro@gmail.com>
2023-11-08 11:10:23 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return(
+  <>
+  <p></p>
+    <Header />
+    <Main />
+    <Footer />
+  </>
+  )
+}
+
+export default App;
